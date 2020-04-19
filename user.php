ec0c9a156a9c13cc47c46d113b550c3e3f6da92e
mambax7 <mambax7@gmail.com>
2020-04-19 09:13:01 -0400

    Undefined class

--- user.php
+++ user.php
@@ -25,6 +25,8 @@
 //  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA //
 //  ------------------------------------------------------------------------ //
 
+use Xmf\Request;
+
 $GLOBALS['xoopsOption']['template_main'] = 'yogurt_user.tpl';
 require __DIR__ . '/header.php';
 
