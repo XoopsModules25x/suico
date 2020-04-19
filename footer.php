8c14177ae0c38c8a20af618c266ca167573297df
Lio <liomj83@gmail.com>
2020-04-19 20:59:16 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -28,11 +28,11 @@ if (false !== stripos($_SERVER['HTTP_USER_AGENT'], 'msie')) {
         XOOPS_URL . '/modules/' . $xoopsModule->getVar('dirname') . '/assets/css/jquery.tabs-ie.css'
     );
 }
-if (stripos($_SERVER['REQUEST_URI'], 'album.php')) {
+//if (stripos($_SERVER['REQUEST_URI'], 'album.php')) {
 $xoTheme->addStylesheet(
     XOOPS_URL . '/modules/' . $xoopsModule->getVar('dirname') . '/assets/css/jquery.lightbox-0.3.css'
 );
-}
+//}
 
 if (!stripos($_SERVER['REQUEST_URI'], 'memberslist.php')) {
 $xoTheme->addScript(
@@ -40,8 +40,9 @@ $xoTheme->addScript(
 );
 }
 
-if (stripos($_SERVER['REQUEST_URI'], 'album.php')) {
-$xoTheme->addScript(XOOPS_URL . '/modules/' . $xoopsModule->getVar('dirname') . '/assets/js/jquery.lightbox-0.3.js'); }
+//if (stripos($_SERVER['REQUEST_URI'], 'album.php')) {
+$xoTheme->addScript(XOOPS_URL . '/modules/' . $xoopsModule->getVar('dirname') . '/assets/js/jquery.lightbox-0.3.js'); 
+//}
 $xoTheme->addScript(XOOPS_URL . '/modules/' . $xoopsModule->getVar('dirname') . '/assets/js/yogurt.js');
 
 if (stripos($_SERVER['REQUEST_URI'], 'memberslist.php')) {
