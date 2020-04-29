3219292ff1ba6c2ce00a38acced22f99853f2cf6
mambax7 <mambax7@gmail.com>
2020-04-28 21:13:28 -0400

    CategoryHandler

---
+++ class/CategoryHandler.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace XoopsModules\Yogurt;
+
+/**
+ * Extended User Profile
+ *
+ * You may not change or alter any portion of this comment or credits
+ * of supporting developers from this source code or any supporting source code
+ * which is considered copyrighted (c) material of the original comment or credit authors.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * @copyright       (c) 2000-2016 XOOPS Project (www.xoops.org)
+ * @license             GNU GPL 2 (http://www.gnu.org/licenses/gpl-2.0.html)
+ * @package             profile
+ * @since               2.3.0
+ * @author              Jan Pedersen
+ * @author              Taiwen Jiang <phppp@users.sourceforge.net>
+ */
+
+/**
+ * @package             kernel
+ * @copyright       (c) 2000-2016 XOOPS Project (www.xoops.org)
+ */
+class CategoryHandler extends \XoopsPersistableObjectHandler
+{
+    /**
+     * @param null|\XoopsDatabase $db
+     */
+
+    public function __construct(\XoopsDatabase $db)
+    {
+        parent::__construct($db, 'yogurt_profile_category', Category::class, 'cat_id', 'cat_title');
+    }
+}
