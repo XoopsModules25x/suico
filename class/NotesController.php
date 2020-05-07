ccdbf9ce424faf9e48902fb72224edbf979614ce
mambax7 <mambax7@gmail.com>
2020-05-07 04:25:24 -0400

    comment out redirect in Controllers

--- class/NotesController.php
+++ class/NotesController.php
@@ -70,9 +70,11 @@ class NotesController extends SuicoController
         if (1 === $this->configsFactory->getCount($criteria)) {
             $configs = $this->configsFactory->getObjects($criteria);
             $config  = $configs[0]->getVar('notes');
+            /*
             if (!$this->checkPrivilegeLevel($config)) {
-                \redirect_header('index.php?uid=' . $this->owner->getVar('uid'), 10, \_MD_SUICO_NOPRIVILEGE);
+                \redirect_header('index.php?uid=' . $this->owner->getVar('uid'), 10, sprintf(_MD_SUICO_NOPRIVILEGE,'Notes'));
             }
+            */
         }
         return true;
     }
